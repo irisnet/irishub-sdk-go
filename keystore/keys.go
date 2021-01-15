791d9bb4bb64d3adae218e3805c55d9e3ee219ce
junjie.chen <xiyueedu@gmail.com>
2021-01-15 14:06:06 +0800

    support keystore (older and newer)

---
+++ keystore/keys.go
@@ -0,0 +1,48 @@
+package keystore
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/irisnet/irishub-sdk-go/crypto"
+	sdksecp256k1 "github.com/irisnet/irishub-sdk-go/crypto/keys/secp256k1"
+	tmcrypto "github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+)
+
+// RecoveryAndExportPrivKeyArmor return the new private key armor(after IrisHubV1.0) from a old keystoreFile(before IrisHubV0.16)
+func RecoveryAndExportPrivKeyArmor(keystore []byte, password string) (armor string, err error) {
+	priv, err := recoveryFromKeyStore(keystore, password)
+	if err != nil {
+		return "", err
+	}
+	return exportPrivKeyArmor(priv, password)
+}
+
+func recoveryFromKeyStore(keystore []byte, auth string) (tmcrypto.PrivKey, error) {
+	if auth == "" {
+		return nil, fmt.Errorf("Password is missing ")
+	}
+
+	var encryptedKey EncryptedKeyJSON
+	if err := json.Unmarshal(keystore, &encryptedKey); err != nil {
+		return nil, err
+	}
+
+	keyBytes, err := decryptKey(&encryptedKey, auth)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(keyBytes) != 32 {
+		return nil, fmt.Errorf("Len of Keyby tes is not equal to 32 ")
+	}
+
+	return secp256k1.PrivKey(keyBytes), nil
+}
+
+func exportPrivKeyArmor(privKey tmcrypto.PrivKey, password string) (armor string, err error) {
+	priv := sdksecp256k1.PrivKey{
+		Key: privKey.Bytes(),
+	}
+	return crypto.EncryptArmorPrivKey(&priv, password, "secp256k1"), nil
+}
--- modules/keys/export.go
+++ modules/keys/export.go
@@ -4,10 +4,13 @@ import (
 	sdk "github.com/irisnet/irishub-sdk-go/types"
 )
 
+// Import: (newKeystore/ privKeyArmor) after IrisHubV1.0
+// ImportFromOlderKeystore: this keystore before IrisHubV0.16
 type Client interface {
 	Add(name, password string) (address string, mnemonic string, err sdk.Error)
 	Recover(name, password, mnemonic string) (address string, err sdk.Error)
 	Import(name, password, privKeyArmor string) (address string, err sdk.Error)
+	ImportFromOlderKeystore(name, password, keystore string) (address string, err sdk.Error)
 	Export(name, password string) (privKeyArmor string, err sdk.Error)
 	Delete(name, password string) sdk.Error
 	Show(name, password string) (string, sdk.Error)
