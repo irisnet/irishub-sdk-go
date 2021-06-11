277b3e34c51f8a0ee664464a7b6fe4e88481364a
Nicke-lucky <13965882371@163.com>
2021-06-11 17:51:30 +0800

    Disassemble the core SDK

---
+++ core-sdk/types/gm_sm2_test.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSm2(t *testing.T) {
+	Sm2()
+}
+
+func TestGenerateKey(t *testing.T) {
+	priv := GenerateKey()
+
+	fmt.Println(priv.Public())
+	fmt.Println(GetPublickey(priv))
+}
