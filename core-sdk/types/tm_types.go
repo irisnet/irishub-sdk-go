fd3ca8ddc0838c570e5bfde17d384a01d6d1811a
Nicke-lucky <13965882371@163.com>
2021-07-02 09:15:36 +0800

    core-sdk fix client

--- core-sdk/types/tm_types.go
+++ core-sdk/types/tm_types.go
@@ -1,12 +1,14 @@
 package types
 
 import (
+	"encoding/hex"
 	cryptoAmino "github.com/irisnet/core-sdk-go/common/crypto/codec"
 	"github.com/irisnet/core-sdk-go/types/kv"
 	"github.com/tendermint/tendermint/crypto"
 	tmbytes "github.com/tendermint/tendermint/libs/bytes"
 	tmclient "github.com/tendermint/tendermint/rpc/client"
 	tmtypes "github.com/tendermint/tendermint/types"
+	"strings"
 )
 
 type (
@@ -24,3 +26,20 @@ type (
 var (
 	PubKeyFromBytes = cryptoAmino.PubKeyFromBytes
 )
+
+func MustHexBytesFrom(hexStr string) HexBytes {
+	v, _ := hex.DecodeString(hexStr)
+	return HexBytes(v)
+}
+
+func HexBytesFrom(hexStr string) (HexBytes, error) {
+	v, err := hex.DecodeString(hexStr)
+	if err != nil {
+		return nil, err
+	}
+	return HexBytes(v), nil
+}
+
+func HexStringFrom(bz []byte) string {
+	return strings.ToUpper(hex.EncodeToString(bz))
+}
